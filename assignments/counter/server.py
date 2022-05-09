b2dd26bae98d70b775710c25444b55b1ac60a5a1
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-05-08 20:29:19 -0700

    initial commit

---
+++ assignments/counter/server.py
@@ -0,0 +1,35 @@
+from tkinter import E
+from flask import Flask, render_template, session, redirect, url_for
+app = Flask(__name__)
+app.secret_key = 'counter' #data encryption key
+
+@app.route('/')
+def index():
+    if 'views' not in session:
+        session['views'] = 0
+    else:
+        session['views'] += 1
+    return render_template('index.html')
+
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return redirect(url_for('index'))
+
+@app.route('/addTwo')
+def addtwo():
+    if 'views' not in session:
+        session['views'] = 0
+    else:
+        session['views'] += 2
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+    
+if (__name__) == '__main__':
+    app.run(debug=True)
+
+
